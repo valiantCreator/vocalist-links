87f49577d00eca889e12d9cd80f22e763e5b849f
David Nana <davidnana.dwomohsarpong@gmail.com>
2026-02-02 03:17:42 -0500

    design: final handwritten x2 branding

--- src/app/s/[id]/page.tsx
+++ src/app/s/[id]/page.tsx
@@ -122,7 +122,7 @@ export default async function SongPage({ params }: PageProps) {
 
         {/* Footer */}
         <p className="pt-8 text-center text-xs font-bold uppercase tracking-widest text-white/20">
-          Nana's Super Sweet 16 <span className="font-great-vibes text-xl lowercase tracking-normal text-white/40 inline-block -rotate-[3deg] translate-y-[2px] ml-1">x2</span>
+          Nana's Super Sweet 16 <span className="font-[family-name:var(--font-great-vibes)] text-xl normal-case tracking-normal inline-block -rotate-[3deg] translate-y-[2px] ml-1">x2</span>
         </p>
       </div>
     </main>
